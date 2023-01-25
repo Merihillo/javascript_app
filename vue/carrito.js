2f218260e89db91c5db04e938bb8ad83eeda3254
AlexisAnteus <alexisperea319@gmail.com>
2023-01-25 10:01:42 -0600

    25/01/23

--- vue/carrito.js
+++ vue/carrito.js
@@ -1,4 +1,4 @@
-Vue.createApp({
+/*Vue.createApp({
     data(){
         return{
             carrito:[],
@@ -14,3 +14,38 @@ Vue.createApp({
       
     }
 }).mount("#app")
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <div id="app">
+       <h1>Lista de carrito</h1>
+       <input v-model ="producto" type="text" placeholder="nombre del producto">
+       <button @click="agregarProducto()">Agregar</button>
+       <button @click="carrito=[]">Limpiar</button>
+
+        <ul>
+            <li v-for="producto in carrito">
+                {{producto}}
+            </li>
+        </ul>
+
+    </div>
+
+
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src ="carrito.js"></script>
+    
+
+
+
+
+</body>
+</html>*/
