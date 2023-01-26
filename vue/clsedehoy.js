f1f9b295a915a02b29b327d5c51da9d4f177c770
AlexisAnteus <alexisperea319@gmail.com>
2023-01-26 14:57:21 -0600

    26/01/23

--- vue/clsedehoy.js
+++ vue/clsedehoy.js
@@ -1,7 +1,25 @@
 Vue.createApp({
     data(){
         return{
+            nombre:'',
+            precio:'',
+            cantidad:'',
+            productos: []
             
+        }
+    },
+    methods: {
+        agregar(){
+            const producto = {
+                nombre: this.nombre,
+                precio: this.precio,
+                cantidad: this.cantidad,
+                tipo: this.tipo,
+                fecha: this.fecha,
+                descripcion: this.descripcion
+            }
+
+            this.productos.push(producto);
         }
     }
 }).mount("#app")
