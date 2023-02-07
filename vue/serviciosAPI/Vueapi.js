60eeba6a2886464cb442160d80876ced91158961
AlexisAnteus <alexisperea319@gmail.com>
2023-02-07 09:51:55 -0600

    amiibos

---
+++ vue/serviciosAPI/Vueapi.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            amiibos: []
+        }
+    },
+    methods: {
+        async obtener() {
+            const data = await fetch("https://amiiboapi.com/api/amiibo/", {
+                method: 'GET'
+
+            });
+            const amiibos = await data.json();
+            this.amiibos = amiibos.amiibo;
+            console.log(this.amiibos);
+        }
+    }
+})
