3b7a75f838cdbaeeeb04e43479894f854be7bc48
AlexisAnteus <alexisperea319@gmail.com>
2023-02-21 09:30:53 -0600

    terminar

--- parcial 2/appi pokemon/app.js
+++ parcial 2/appi pokemon/app.js
@@ -1,9 +1,12 @@
 Vue.createApp({
     data(){
         return{
-            pokemon: null
+            pokemones: []
 
         }
+    },
+    mounted() {
+        this.getPokemon();
     },
     methods: {
         async getPokemon(){ //async funcion acirncrona, le dices que no importa que no se quede estancada la pagina
@@ -11,15 +14,22 @@ Vue.createApp({
             //POST (PARA CREAR RECURSOS)
             //PUT (MODIFICAR RECURSOS)
             //DELETE (ELIMINAR RECURSOS)
-            const random = Math.floor (Math.random() * 200);
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon/' +random,{
-                method: 'GET'
-            });
+            //const random = Math.floor (Math.random() * 200); +random
 
-            const data = await response.json();
-            //console.log(data);
-            this.pokemon=data;
+            var i = 1;
 
+            while (i <= 200)
+            {
+                const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + i,{
+                    method: 'GET'
+                });
+                const pokemon = await response.json();
+                 //console.log(data);
+                this.pokemon.push(pokemon);
+
+                 i++;
+
+             }
 
         }
     }
